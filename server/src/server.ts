cee0c3662c6674dd7757b6fee184a8cff9e3e465
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-05-08 19:15:40 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 
 import { routes } from './routes';
@@ -5,6 +6,7 @@ import logger from './utils/logger';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
