8205b94446dc8b4c35ff876456c2bbc2d8aa404d
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-05-08 19:15:13 -0300

    Create submit feedback use case

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,3 +1,5 @@
+import { throws } from 'assert';
+
 import { IMailAdapter } from '../adapters/mail-adapter';
 import { IFeedbacksRepository } from '../repositories/feedbacks-repository';
 
@@ -14,6 +16,18 @@ export class SubmitFeedbackUseCase {
   ) {}
 
   async execute({ comment, screenshot, type }: ISubmitFeedbackUseCaseRequest) {
+    if (!type) {
+      throw new Error('Type is required.');
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required.');
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.');
+    }
+
     await this.feedbacksRepository.create({
       comment,
       screenshot,
