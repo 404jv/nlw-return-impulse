3cfd392b53a44550c10e3582e2d441aa51d655c5
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-05-26 23:05:15 -0300

    Use hook useCachedResources

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,18 +4,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
-
-import { 
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium
-} from '@expo-google-fonts/inter';
+import useCachedResources from './src/hooks/useCachedResources';
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   Inter_400Regular,
-  //   Inter_500Medium,
-  // });
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  }
 
   return (
     <View style={{
